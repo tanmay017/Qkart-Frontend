27b9f151b317708114db712adca88d75a4bcd777
Nabhan Abdulla <nabhan@criodo.com>
2021-01-07 20:10:26 +0550

    updated changes - 2021-01-07 19:50:26

--- setup.sh
+++ setup.sh
@@ -1,3 +1,9 @@
+# Store workspace public IP to config file
+IP_VAR="$(curl http://checkip.amazonaws.com/)"
+CONFIG='{"workspaceIp": "'"$IP_VAR"'"}'
+echo $CONFIG > src/ipConfig.json
+
+# Install project dependencies
 rm -rf ~/node_modules
 rm -rf node_modules
 rm -rf ~/.npm 
